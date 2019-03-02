47c3a918b62291dc71a65022f302161ac81ef05f
Muragijimana <beastar457@gmail.com>
2019-03-02 12:03:58 +0200

    initial

---
+++ src/main/java/com/enexus/letter_in_circle/LetterInCircle.java
@@ -0,0 +1,12 @@
+package com.enexus.letter_in_circle;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class LetterInCircle {
+  public static void s(Context c, String message){
+
+    Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+  }
+}
